805704b2783ae2a45aa392878ab69ed21831ddfc
primus <primus@localhost.localdomain>
2021-12-28 23:37:19 -0500

    Function updateBook created.

--- main.go
+++ main.go
@@ -64,7 +64,21 @@ func createBook(w http.ResponseWriter, r *http.Request){
 }
 //To update a book.
 func updateBook(w http.ResponseWriter, r *http.Request){
-	 
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books{ 
+		if item.ID == params["id"]{
+		books = append(books[:index],books[index+1:]...)
+		var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID =   params["id"]
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+	return
+		}
+
+	}
+	json.NewEncoder(w).Encode(books)	 
 
 }
 //To delete a book
