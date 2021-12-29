8635735704e6bb6b2c49c8ffa882d15df261e4d9
primus <primus@localhost.localdomain>
2021-12-28 22:29:22 -0500

    Second commit.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mux1
+Authors' Credits:
+1. https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA
+
--- main.go
+++ main.go
@@ -10,5 +10,14 @@ import(
 )
 
 func main(){ 
-	
+	// Initialize the router
+	router := mux.NewRouter()
+
+	//Handling Router: Establishing Endpoints
+
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
+	router.HandleFunc("/api/books", createBooks).Methods("POST")
+	router.HandleFunc("/api/books{id}", updateBooks).Methods("PUT")
+	router.HandleFunc("/api/books{id}", deleteBook).Methods("DELETE")
 }
